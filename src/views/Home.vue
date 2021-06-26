ba2ca5ff99412f37f3b78972d469150a2b943f3d
Veeral Jetani <veeral@orderup.com.au>
2021-06-26 23:08:22 +1000

    added styling and solved issue with tags to limit the tags to 3

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="home">
-    <form>
-      <label>Search: <input v-model="tag" type="text">
-      </label>
-      <button type="submit" class="search-btn" @click.prevent="search">Search</button>
-    </form>
-    <p v-if="loading">Loading...</p>
-    <ul v-else>
+    <nav class="navbar">
+      <form class="searchbar">
+        <label>
+          <span class='screen-reader-only'>Search:</span>
+          <input v-model="tag" placeholder="Search for photos" type="text" class="searchbar-input">
+        </label>
+        <button type="submit" class="btn btn--green btn--go" @click.prevent="search">Search</button>
+      </form>
+    </nav>
+    <div class="wrapper">
+    <p v-if="loading" class="text-centered">Loading...</p>
+    <ul class="image-card-grid" v-else>
       <image-card v-for="image in images" :key="image.id" :image="image"></image-card>
     </ul>
+    </div>
   </div>
 </template>
 
@@ -62,3 +68,77 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+.screen-reader-only {
+  height: 1px;
+  width: 1px;
+  position: absolute;
+  left: -100000px;
+}
+.text-centered {
+  text-align: center;
+}
+.wrapper {
+  margin: 0 auto;
+  max-width: 800px;
+  @media only screen and (max-width: 799px) {
+    max-width: 100%;
+    margin: 0 1.5rem;
+  }
+}
+.image-card-grid {
+  list-style: none;
+  margin: .5rem 0;
+  padding: 0;
+  display: flex;
+  align-items: flex-start;
+  flex-wrap: wrap;
+}
+.navbar {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem;
+  background: #F0F0F0;
+}
+.searchbar {
+  width: 300px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  @media only screen and (max-width: 549px) {
+    width: 100%;
+    label {
+      width: 80%;
+    }
+  }
+}
+.searchbar-input {
+  padding: .5rem 1rem;
+  border-radius: 20px;
+  font-size: 1rem;
+  border: 1px solid gray;
+  min-width: 300px;
+  @media only screen and (max-width: 549px) {
+    min-width: 0;
+    width: 100%;
+  }
+}
+.btn {
+  padding: .5rem 1rem;
+  font-size: 1rem;
+  border-radius: 20px;
+  background: transparent;
+  border: none;
+}
+.btn--green {
+  background: #42b983;
+  color: white;
+  font-weight: bold;
+}
+.btn--go {
+  padding: .5rem 2rem;
+  margin-left: 1rem;
+}
+</style>
